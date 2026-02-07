c06e42fcc198dda7348a2b06559e6b5aa3d6330d
ivanclemente2525-source <ivanclemente2525@gmail.com>
2026-02-07 17:39:51 +0800

    Add files via upload

---
+++ ComputeAverageProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter 5 grades seperated by new line");
+
+            double grade1 = Convert.ToDouble(Console.ReadLine());
+            double grade2 = Convert.ToDouble(Console.ReadLine());
+            double grade3 = Convert.ToDouble(Console.ReadLine());
+            double grade4 = Convert.ToDouble(Console.ReadLine());
+            double grade5 = Convert.ToDouble(Console.ReadLine());
+
+            double avarege = (grade1 + grade2 + grade3 + grade4 + grade5) / 5;
+            double aroundavarage = Math.Round(avarege);
+
+            Console.WriteLine(" The Total Average is " + avarege + " and around of " + aroundavarage);
+            Console.WriteLine("Press any key to exit. . . ");
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
---
+++ MathProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MathApp
+{
+    class MathProgram
+    {
+
+        static void Main(string[] args)
+        {
+            double num1 = 9.21;
+            double num2 = 11.0;
+            double baseNum = 2.0;
+            double exponent = 3.0;
+
+            Console.WriteLine("The Absolute value of " + num2 + " is " + Math.Abs(num2));
+            Console.WriteLine("The Higher value between " + num1 + " and 10 is " + Math.Max(num1, 10));
+            Console.WriteLine(baseNum + " to the power of " + exponent + " is " +Math.Pow(baseNum, exponent));
+            Console.WriteLine("The square root of 150 is " + Math.Sqrt(150));
+            Console.WriteLine("The celling value of " + num1 + " is " + Math.Ceiling(num1));
+
+            Console.WriteLine("\nPress any key to exit. . .");
+            Console.ReadKey();
+
+
+
+        }   
+    }
+}
