f614a518ef791c26de44521bc0b0aeb0d471f344
ivanclemente2525-source <ivanclemente2525@gmail.com>
2026-02-28 17:55:25 +0800

    Add files via upload

---
+++ frmStudentGradeProgram.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace StudentGradeApplication
+{
+    partial class frmStudentGradeProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.Englishtxtbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Mathtxtbox = new System.Windows.Forms.TextBox();
+            this.Sciencetxtbox = new System.Windows.Forms.TextBox();
+            this.Filipinotxtbox = new System.Windows.Forms.TextBox();
+            this.Historytxtbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.Generateava = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StudentGradeApplication.Properties.Resources.Screenshot_28_2_2026_151919_www_bing_com;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(885, 537);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.Location = new System.Drawing.Point(290, 114);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(285, 20);
+            this.Nametxt.TabIndex = 1;
+            // 
+            // Englishtxtbox
+            // 
+            this.Englishtxtbox.Location = new System.Drawing.Point(290, 157);
+            this.Englishtxtbox.Name = "Englishtxtbox";
+            this.Englishtxtbox.Size = new System.Drawing.Size(120, 20);
+            this.Englishtxtbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NAME: ";
+            // 
+            // Mathtxtbox
+            // 
+            this.Mathtxtbox.Location = new System.Drawing.Point(290, 183);
+            this.Mathtxtbox.Name = "Mathtxtbox";
+            this.Mathtxtbox.Size = new System.Drawing.Size(120, 20);
+            this.Mathtxtbox.TabIndex = 4;
+            // 
+            // Sciencetxtbox
+            // 
+            this.Sciencetxtbox.Location = new System.Drawing.Point(290, 209);
+            this.Sciencetxtbox.Name = "Sciencetxtbox";
+            this.Sciencetxtbox.Size = new System.Drawing.Size(120, 20);
+            this.Sciencetxtbox.TabIndex = 5;
+            // 
+            // Filipinotxtbox
+            // 
+            this.Filipinotxtbox.Location = new System.Drawing.Point(290, 235);
+            this.Filipinotxtbox.Name = "Filipinotxtbox";
+            this.Filipinotxtbox.Size = new System.Drawing.Size(120, 20);
+            this.Filipinotxtbox.TabIndex = 6;
+            // 
+            // Historytxtbox
+            // 
+            this.Historytxtbox.Location = new System.Drawing.Point(290, 261);
+            this.Historytxtbox.Name = "Historytxtbox";
+            this.Historytxtbox.Size = new System.Drawing.Size(120, 20);
+            this.Historytxtbox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(323, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Grades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(228, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 14);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "English";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(228, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 14);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Math";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(228, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 14);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Science";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(228, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 14);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "FIlipino";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(228, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 14);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "History";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.Font = new System.Drawing.Font("MS PGothic", 17F, System.Drawing.FontStyle.Bold);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblStatus.Location = new System.Drawing.Point(461, 202);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 23);
+            this.lblStatus.TabIndex = 15;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.BackColor = System.Drawing.Color.Transparent;
+            this.lblAverage.Font = new System.Drawing.Font("MS PGothic", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAverage.Location = new System.Drawing.Point(461, 202);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 23);
+            this.lblAverage.TabIndex = 16;
+            // 
+            // Generateava
+            // 
+            this.Generateava.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold);
+            this.Generateava.Location = new System.Drawing.Point(341, 287);
+            this.Generateava.Name = "Generateava";
+            this.Generateava.Size = new System.Drawing.Size(180, 23);
+            this.Generateava.TabIndex = 17;
+            this.Generateava.Text = "Generate Average";
+            this.Generateava.UseVisualStyleBackColor = true;
+            this.Generateava.Click += new System.EventHandler(this.Generateava_Click);
+            // 
+            // frmStudentGradeProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 536);
+            this.Controls.Add(this.Generateava);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Historytxtbox);
+            this.Controls.Add(this.Filipinotxtbox);
+            this.Controls.Add(this.Sciencetxtbox);
+            this.Controls.Add(this.Mathtxtbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Englishtxtbox);
+            this.Controls.Add(this.Nametxt);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmStudentGradeProgram";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.TextBox Englishtxtbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Mathtxtbox;
+        private System.Windows.Forms.TextBox Sciencetxtbox;
+        private System.Windows.Forms.TextBox Filipinotxtbox;
+        private System.Windows.Forms.TextBox Historytxtbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button Generateava;
+    }
+}
+
